8999cf69c2b1427e12eb740a2c0ca90c0334c6ca
jtumms <john.tumminelli@tummsmedia.com>
2016-11-08 10:19:18 -0500

    Initial upload with animation

---
+++ src/com/theironyard/Ant.java
@@ -0,0 +1,31 @@
+package com.theironyard;
+
+/**
+ * Created by john.tumminelli on 11/8/16.
+ */
+public class Ant {
+    double x;
+    double y;
+
+
+    public Ant(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
